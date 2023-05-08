15447125d2994a5afcd766d5b4ad5919d4b006e4
João Lucas Crescencio Granjeiro Barbosa <joaolucascgbarbosa878@gmail.com>
2023-05-08 14:04:00 -0300

    criação de rota de login com autenticação no back end

--- back-end/main.js
+++ back-end/main.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
     res.send("Hello world");
 });
 
-app.listen(300, () => {
+app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
--- back-end/routes.js
+++ back-end/routes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const routes = express.Router();
+const users = [{
+    id : 1,
+    nome : "João Lucas",
+    email : "joaolucascgbarbosa878@gmail.com",
+    senha : c123456
+}]
 
 routes.post('/login', (req, res) => {
-    res.send ('Login endpoint')
+    const {email, senha} = req.body;
+    const user  = users.find(user => user.email === email && user.senha === senha);
+    if (user){
+        return res.status(200).json(user)
+    } else {
+        return res.status(401).json({message: "Credenciais inválidas"});
+    } 
+    
 });
 module.exports = routes;
